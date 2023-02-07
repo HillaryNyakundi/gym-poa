c6053ee3fa7efb0af82f57ce24a773872df736d7
Hillary Nyakundi <hillarynyakundi66@gmail.com>
2023-02-07 14:28:25 +0300

    Today's changes

---
+++ src/components/MainHeader.jsx
@@ -0,0 +1,19 @@
+
+
+const Header = () => {
+  return (
+    <header className="main__header">
+      <div className="container main__header-container">
+        <div className="main__header-left">
+          <h4>#100DayOfWorkOut</h4>
+          <h1>Join The Legends Of The Fitness World</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam porro repellendus sapiente veritatis, facilis aperiam?</p>
+          
+        </div>
+        <div className="main__header-right"></div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
